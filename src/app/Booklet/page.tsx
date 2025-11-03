0956d373036d1909244dc003cc14b41e89722e56
Lucas Rodrigues <lucas.ramos@stellantis.com>
2025-11-03 09:55:59 -0300

    chore: centralizing the pdf

--- src/app/Booklet/page.tsx
+++ src/app/Booklet/page.tsx
@@ -5,25 +5,24 @@ import Footer from "@/components/Footer/Footer";
 import NavBar from "@/components/NavBar/NavBar";
 import dynamic from 'next/dynamic';
 
-const PDFViewer = dynamic(() => 
-import('@/components/PDFViewer/PDFViewer'), { ssr: false });
+const PDFViewer = dynamic(() =>
+    import('@/components/PDFViewer/PDFViewer'), { ssr: false });
 
 export default function About() {
-    
+
     return (
         <>
             <NavBar />
-            <Hero>
+                <Hero>
+                <div className=''>
+                    <PDFViewer />
+                </div>
 
-            <div className='w-screen'>
-                <PDFViewer />
-            </div>
+                <div className=" p-3 rounded-3xl bg-red-600/70 drop-shadow-xs drop-shadow-black/50 text-white space-y-4 text-center indent-2">
+                    <a href='\cartilha-nutri-stellantis.pdf' download>BAIXE NOSSA CARTILHA</a>
+                </div>
 
-            <div className=" p-3 rounded-3xl bg-red-600/70 drop-shadow-xs drop-shadow-black/50 text-white space-y-4 text-center indent-2">
-                <a href='\cartilha-nutri-stellantis.pdf' download>BAIXE NOSSA CARTILHA</a>
-            </div>
-
-            </Hero>
+                </Hero>
             <Footer />
         </>
     );
